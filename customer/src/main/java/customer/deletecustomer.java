7550586249881a7f27431bae5561acb251ae4aba
ranjithmn <109076718+ranjithmn@users.noreply.github.com>
2022-07-27 14:28:53 +0530

    Update deletecustomer.java

--- customer/src/main/java/customer/deletecustomer.java
+++ customer/src/main/java/customer/deletecustomer.java
@@ -1,10 +1,29 @@
 package customer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class deletecustomer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("uday");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		
+		Customer c=entityManager.find(Customer.class, 103);
+		if(c!=null)
+		{
+		entityTransaction.begin();
+		entityManager.remove(c);
+		entityTransaction.commit();
+			
+		}
+				
 
 	}
 
 }
+
